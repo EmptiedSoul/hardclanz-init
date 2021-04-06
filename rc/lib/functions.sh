e336711a8f08add69453f931e840e38b039cf1eb
emptiedsoul <emptiedsoul@hardclanz.site>
2021-04-06 20:12:08 +0300

    Add sane runlevels, add eval_retval

--- rc/lib/functions.sh
+++ rc/lib/functions.sh
@@ -6,12 +6,19 @@ YELLOW_COLOR="\e[1;33m"
 BLUE_COLOR="\e[1;34m"
 CLEAR_COLOR="\e[0m"
 
+is_true(){
+	case $1 in 
+		Y*|y*|on|true|OK|ok|1)	return 0;;
+		*)			return 1;;
+	esac
+}
+
 run_daemon(){
 	exec runuser -u ${RUN_DAEMON_USER:-root} -- $*
 }
 
 msg_ok(){
-	echo -e "[ ${GREEN_COLOR}OK${CLEAR_COLOR} ] $*"
+	echo -e "[ ${GREEN_COLOR}OK${CLEAR_COLOR} ] $*" 
 }
 
 msg_pending(){
@@ -28,4 +35,15 @@ msg_warn(){
 
 msg_info(){
 	echo -e "[${BLUE_COLOR}INFO${CLEAR_COLOR}] $*"
+}
+
+eval_retval(){
+	local retval=${?}
+	case $retval in
+		$SUCCESS) msg_ok   "$SUCCESS_MSG";;
+		$WARN)	  msg_warn "$WARN_MSG";;
+		$FAIL)    msg_fail "$FAIL_MSG";;
+	esac
+	is_true "$EVAL_EXIT" && exit $retval
+	return 0
 }
